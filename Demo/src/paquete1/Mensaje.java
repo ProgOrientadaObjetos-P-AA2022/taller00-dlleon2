c8fa21c3a42ff7c7c4e094e984daaa7bc50e75dc
dlleon2 <dlleon2@utpl.edu.ec>
2022-04-11 09:42:23 -0500

    primer cambio en git

--- Demo/src/paquete1/Mensaje.java
+++ Demo/src/paquete1/Mensaje.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package paquete1;
 
-/**
+package paquete1;
+import paquete4.Palabra;
+/**}
  *
  * @author reroes
  */
